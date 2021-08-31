2bb404f466b2f6fd928cf13b67baa265bc3efbac
1 <2>
2021-08-31 17:18:40 +0800

    init

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-13 17:47:56
+ * @LastEditTime: 2021-08-13 17:48:05
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \nuxt\commitlint.config.js
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] };
