1001d2f6be113e412ae382f173d691ab143a7b6d
1 <2>
2021-08-31 17:23:42 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2021-08-31 17:19:51
- * @LastEditTime: 2021-08-31 17:19:52
+ * @LastEditTime: 2021-08-31 17:23:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \test\src\main.js
  */
+console.log(2222222);
