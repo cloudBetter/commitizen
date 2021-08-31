3aab43ed3dbaf1f0bca362f07ad04f2217adcaff
1 <2>
2021-08-31 17:24:08 +0800

    init

--- husky.config.js
+++ husky.config.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-08-31 16:22:54
- * @LastEditTime: 2021-08-31 16:32:56
- * @LastEditors: your name
+ * @LastEditTime: 2021-08-31 17:23:54
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \nuxt-blog\husky.config.js
  */
 module.exports = {
   "hooks": {
-    "pre-commit": "lint-staged -c lint-staged.config.js",
+
     "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
     "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
   }
